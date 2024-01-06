15ce1abe7048fc062cd27fdab5a6c69c15f2f338
miguel <80590513+Lawnservices@users.noreply.github.com>
2024-01-06 16:53:56 -0500

    goo-header

--- scritp.js
+++ scritp.js
@@ -1,11 +1,16 @@
 const casa = document.getElementById('navbar');
+const casas = document.getElementById('verde');
 
 document.addEventListener('scroll',() => {
   var  scroll_position = window.scrollY;
   if(scroll_position > 100){
     navbar.style.backgroundColor= '#333';
+    verde.style.color = 'green';
+    verde.style.marginTop = '100px';
   } else{
     navbar.style.backgroundColor= '#003349';
+    verde.style.color = '#000';
+     verde.style.marginTop = '24px';
   }
    
 });
